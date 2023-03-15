f34b71d5406dbd4d8c2d2fc07a513a8fafeaec52
ErisBackend <eris7tompul@gmail.com>
2023-03-15 16:02:00 +0700

    study case jd sederhana

---
+++ pertemuan-3/JS/dasar.js
@@ -0,0 +1,115 @@
+// javascript
+// console.log artinya fungsi manfaatnya untuk memunculkan kode kedalam console di browser
+
+// tipe data pada JS
+// 1.string
+// string berarti huruf
+// contoh: 
+//     console.log("makanan")
+//     console.log("minuman")
+// 2.Number
+// contoh:
+//     console.log("10")
+//     console.log("10.5")
+//     console.log("0.5")
+// Boolean
+// contoh:
+//     console.log(false)
+//     console.log(true)
+
+// dan masih banyak lagi type data lainya seperti Array, object,promise,dll
+
+// Aritmatika
+// contoh: +,-,*,/ 
+
+// penggunaanya
+// console.log("1+1")
+// console.log("1-1")
+// console.log("1*1")
+// console.log("1/1")
+// console.log("1%1")
+
+// ada juga concatenate
+// contoh:
+// console.log("1+makana")
+// console.log("hallo dunia")
+
+// penggunaan tanda kutip 1 dan 2 pada console.dog
+// String
+// console.log("haru jum'at")
+// console.log('dia anak laki"')
+// console.log('hari ini hari jum'\at,\ndan saya berkata"hallo dunia"')
+
+// variabel
+
+// teampat untuk menampung tipe data sementara
+// contoh:
+
+let tiga = 3.121292
+let angka1 = 10
+let angka2 = 20
+
+let hasil = (angka1 + angka2)
+console.log(angka1 + '+'+ angka2  +'=',hasil)
+
+
+
+let tes = 12 
+// cara sederhananya
+// tes = tes + 2
+// operator penugasan
+tes += 3
+// tes -= 4
+// tes /=5
+// tes %=6
+
+console.log(tes)
+
+// operasi logika
+
+console.log (3>2)
+// console,log (3<2)
+// console,log (3==2)
+// console,log (3==3)
+// console,log (3=='3')
+// console,log (3==='3')
+// console,log (3 !='3')
+// console,log (3!=='3')
+
+// console,log (3>=2)
+// console,log (3<=2)
+// console,log (3<=3)
+// console,log (3>=3)
+
+// dan ,atau,bukan
+// && artinya dan , || artinya atau , ! artiya bukan
+// contoho &&:
+console.log(true && true)
+// console.log(true && false)
+// console.log(false && false)
+// console.log(false && true)
+
+// contoh ||:
+// console.log(false || true)
+// console.log(false || true)
+// console.log(false || true)
+console.log(false || true)
+
+// contoh ! :
+// console.log(false !true)
+//console.log(false !true)
+
+// console.log(false && true && false)
+// console.log(false || true || false)
+// console.log((false && true && false))||((true && true))
+
+// // dialog box
+// alert("halo dunia")
+// let tes1 = parseInt(prompt("masukkan angka 1")) ;//menghasilkan string
+// let tes1 = parseInt(prompt("masukkan angka 2")) ;//menghasilkan string
+// alert (tes1+tes2);
+
+// let tes = confirm("apa");
+// console.log(tes);
+
+
---
+++ pertemuan-3/JS/if.js
@@ -0,0 +1,28 @@
+// kondisi
+// if((3>1&&!1<1))||3>0||2==1){
+//     console.log("benar");
+// }else{
+//     console.log("ini salah")
+// }
+
+// let a = 100
+// isFinite(a ===8){
+//     console.log("ini delapan");
+// }else if(a===1){
+//     console.log("ini pertama")
+// }else if(a<10){
+//     console.log("pefect")
+// }else {
+//     console.log("saya gk tau")
+// }
+
+// switch(a){
+//     case 1:
+//         console.log('ini satu');
+//         break;
+//     case 10:
+//         console.log("ini tes");
+//     default:
+//         console.log("ok")
+// }
+
